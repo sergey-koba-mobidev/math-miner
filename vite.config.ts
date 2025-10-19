2a7b67b23fa709fa170f0b2e8b73ceccd3738eb0
Sergey <s.koba@mobidev.biz>
2025-10-19 21:45:14 +0200

    feat: Integrate PWA features and update assets
    Adds PWA capabilities to Math Miner, enabling offline support and app installation. This includes:
    - Configuring Vite PWA plugin with manifest and asset includes.
    - Adding a PWA assets generator script and configuration.
    - Updating application version and dependencies in package.json.
    - Including updated logo assets for PWA icons.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,31 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: 'math-miner',
-  plugins: [react()],
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Math Miner',
+        short_name: 'Math Miner',
+        description: 'Your mission is to dig as deep as you can! Uncover rare treasures, defeat powerful monsters, and upgrade your equipment to become the ultimate Math Miner.',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
